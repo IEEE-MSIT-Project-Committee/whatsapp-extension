e31a48e80f94e6c2ecdecab27aa5a1b76eb8e716
Utkarsh Ahuja <ahujautkarsh80@gmail.com>
2022-06-30 21:35:06 +0530

    files

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', goapp);      
+});
+
+function goapp()
+{
+  let nummer= document.getElementById("abcd").value ;
+  nummer=nummer.toString();
+// nummer = nummer.replace(/\D/g,'');
+// nummer = nummer.replace(/^0+/, '');
+
+const link="https://api.whatsapp.com/send?phone="+nummer;
+chrome.tabs.create({url:link});
+
+//  window.location = 'https://api.whatsapp.com/send?phone=' + nummer;
+return false;}
