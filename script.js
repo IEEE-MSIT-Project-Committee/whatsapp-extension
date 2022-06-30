fe8a6de9df71db8e155518e833eeb64588198e34
Utkarsh Ahuja <ahujautkarsh80@gmail.com>
2022-06-30 22:07:48 +0530

    stylr

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function goapp()
 {
-  let nummer= document.getElementById("abcd").value ;
+  let nummer= document.getElementById("number").value ;
   nummer=nummer.toString();
-// nummer = nummer.replace(/\D/g,'');
-// nummer = nummer.replace(/^0+/, '');
+nummer = nummer.replace(/\D/g,'');
+nummer = nummer.replace(/^0+/, '');
 
 const link="https://api.whatsapp.com/send?phone="+nummer;
 chrome.tabs.create({url:link});
